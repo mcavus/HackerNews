55920a77311206fa09d9aa239e62208347bbc2b7
Muhammed Cavusoglu <mcavus495@gmail.com>
2018-08-17 16:39:12 +0300

    Initial Commit

---
+++ HackerNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackerNews
+//
+//  Created by Muhammed Cavusoglu on 17.08.2018.
+//  Copyright © 2018 Muhammed Cavusoglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
